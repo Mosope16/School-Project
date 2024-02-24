c3f8f158c1afbbb9bebf3dddb19e702762d4d740
Mosope16 <115270838+Mosope16@users.noreply.github.com>
2024-02-24 04:06:24 +0100

    Add files via upload

---
+++ lowerToUpper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+//Defining the uppercase_letter Function
+void uppercase_letter(char lowercase_letter[]){
+    int i = 0;
+    while (lowercase_letter[i] != '\0') {
+        if (lowercase_letter[i] >= 'a' && lowercase_letter[i] <= 'z'){
+            lowercase_letter[i] = lowercase_letter[i] - 32;
+        }
+        i++;
+    }        
+}
+ 
+int main(){
+    char lowercase_letter[25];
+
+    printf("Enter a lowercase letter: \n");
+    fgets(lowercase_letter, sizeof(lowercase_letter), stdin);
+
+    uppercase_letter(lowercase_letter);
+
+    printf("The uppercase is: %s\n", lowercase_letter);
+
+    return 0;
+}
